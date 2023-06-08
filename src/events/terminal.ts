ab702e24e954b1ed7252dd07401c02557a7de151
Semmy <semmywong@126.com>
2023-06-08 14:15:21 +0800

    feat: support ssh terminal

--- src/events/terminal.ts
+++ src/events/terminal.ts
@@ -2,11 +2,12 @@
  * @Author: Semmy Wong
  * @Date: 2023-02-21 13:10:36
  * @LastEditors: Semmy Wong
- * @LastEditTime: 2023-06-08 10:54:03
+ * @LastEditTime: 2023-06-08 14:14:23
  * @Description: 描述
  */
 import * as vscode from 'vscode';
 import { ServerNode } from '../ServerNode';
+import localize from '../localize';
 
 let loading = false;
 
@@ -15,35 +16,15 @@ let loading = false;
  * @param serverNode
  */
 export const terminal = (serverNode: ServerNode) => {
-  // const bash = vscode.window.createTerminal({ name: serverNode.label, hideFromUser: true });
-  // bash.show();
-  const panel = vscode.window.createWebviewPanel('terminal', 'Terminal', vscode.ViewColumn.One, {});
-
-  const terminal = vscode.window.createTerminal({
-    name: 'My Terminal',
-  });
-  panel.webview.onDidReceiveMessage((message) => {
-    if (message.command === 'write') {
-      terminal.sendText(message.text);
-    }
-  });
-
-  panel.webview.html = `
-  <html>
-  <body>
-      <div id="terminal"></div>
-      <script>
-          const terminal = new Terminal();
-          terminal.open(document.getElementById('terminal'));
-          const write = function(data) {
-              vscode.postMessage({
-                  command: 'write',
-                  text: data
-              });
-          };
-          terminal.onData(write);
-      </script>
-  </body>
-  </html>
-`;
+  const { config } = serverNode;
+  if (!config.privateKey) {
+    vscode.window.showInformationMessage(
+      localize('ext.terminal.needPrivateKeyCertificate'),
+      localize('common.titleTip'),
+    );
+    return;
+  }
+  const bash = vscode.window.createTerminal({ name: serverNode.label, hideFromUser: true });
+  bash.sendText(`ssh -i ${config.privateKey} ${config.username}@${config.host}`);
+  bash.show();
 };
